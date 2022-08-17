a7159b4619c1b1d11898a2037376f06b2884aa95
cagatay hanedan <cagatayhanedan@gmail.com>
2022-08-17 14:01:28 +0300

    updated wasm path

--- bundle.js
+++ bundle.js
@@ -121469,7 +121469,7 @@ viewer.grid.setGrid();
 viewer.axes.setAxes();
 
 async function loadIfc(url) {
-    await viewer.IFC.setWasmPath("../");
+    await viewer.IFC.setWasmPath("/");
     const model = await viewer.IFC.loadIfcUrl(url);
     viewer.shadowDropper.renderShadow(model.modelID);
 }
